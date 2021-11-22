25382aebd5cb5528718f9fd50943e6e9ea721945
Bruno Bertechini <bruno.bertechini@outlook.com>
2021-11-22 17:01:34 +1100

    fixed mysqlConnect.php

--- config/test/mysqlConnect.php
+++ config/test/mysqlConnect.php
@@ -1,4 +1,14 @@
 <?php
+
+// $host = '<yourservername>.mysql.database.azure.com';
+// $username = '<yourusername>@<yourservername>';
+// $password = '<yourpassword>';
+// $db_name = 'testdb';
+$host = '';
+$username = '';
+$password = '';
+$db_name = '';
+  
 foreach ($_SERVER as $key => $value) {
   if (strpos($key, "MYSQLCONNSTR_") !== 0) {
       continue;
@@ -10,17 +20,15 @@ foreach ($_SERVER as $key => $value) {
   // $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 
   $host = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-  $username = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-  $password = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-  $db_name = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+  $db_name = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+  $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+  $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-// $host = '<yourservername>.mysql.database.azure.com';
-// $username = '<yourusername>@<yourservername>';
-// $password = '<yourpassword>';
-// $db_name = 'testdb';
-
- echo "mysqlnd_azure.enableRedirect: ", ini_get("mysqlnd_azure.enableRedirect"), "\n";
+ echo "mysqlnd_azure.enableRedirect: ", ini_get("mysqlnd_azure.enableRedirect"), "\n<br />";
+ echo "host: ", $host, " username: ", $username, " dbName: ", $db_name, "\n<br />";
+ echo "\n<br />";
+ 
  $db = mysqli_init();
  //The connection must be configured with SSL for redirection test
  $link = mysqli_real_connect ($db, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
