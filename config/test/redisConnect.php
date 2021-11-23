26848d79aa6e163b19d57f2e761fd3f4d64ee269
Bruno Bertechini <bruno.bertechini@outlook.com>
2021-11-23 12:57:18 +1100

    added php-redis x86 and fixed php.ini

---
+++ config/test/redisConnect.php
@@ -0,0 +1,32 @@
+<?php
+
+//
+// Configure Redis Cache
+//
+$redis_host = '';
+$redis_port = '';
+$redis_password = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "CUSTOMCONNSTR_redis") !== 0) {
+        continue;
+    }
+    
+    $redis_host = preg_replace("/^.*Host=(.+?);.*$/", "\\1", $value);
+    $redis_port = preg_replace("/^.*Port=(.+?);.*$/", "\\1", $value);
+    $redis_password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+define( 'WP_REDIS_HOST', $redisHost );
+define( 'WP_REDIS_PORT', $redisPort );
+define( 'WP_REDIS_PASSWORD', $redisPassword );
+define( 'WP_REDIS_CLIENT', 'phpredis' );
+define( 'WP_REDIS_TIMEOUT', 1 );
+define( 'WP_REDIS_READ_TIMEOUT', 1 );
+define( 'WP_REDIS_DATABASE', 0 );
+
+echo "Redis Host: ", $redis_host, "\n< br/>";
+echo "Redis Port: ", $redis_port, "\n< br/>";
+echo "Redis Password: ", $redis_password, "\n< br/>";
+
+?>
